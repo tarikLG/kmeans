0b7a9cf1cafeaebb1a37ae1d8ae5fe22b3489ab0
tarikLG <tarikglul@gmail.com>
2022-06-16 20:15:23 -0400

    suggestions

--- kmean.py
+++ kmean.py
@@ -1,53 +1,53 @@
-import random
-import imageio
+from PIL import Image
+import numpy as np
 
-def euclid_dist(a, b):
-    distance = ((a[0] - b[0])**2) + ((a[1] - b[1])**2) + ((a[2] - b[2])**2)
+class KMean:
+    def __init__(self, k, s):
+        self.k = k
+        self.s = s
 
-    return distance
 
-def calc_clusterCent(x, k):
-    calculated = []
-    center = [0, 0, 0]
-    for c in x:
-        for p in c:
-            center[0] += p[0]
-            center[1] += p[1]
-            center[2] += p[2]
-        for n in center:
-            center[center.index(n)] = int(n/len(c))
-        calculated.append(center)
+    def clustering(self):
+        check = True
+        clusterCen = np.array(self.s[np.random.randint(self.s.shape[0], size=self.k)])
+
+        while check: 
+            clusters = []
+
+            for i in range(self.k):
+                clusters.append([])
+
+            for point in self.s.tolist():
+                point = np.array(point)
+                temp = np.sum(np.square(clusterCen - point), axis=1)
+                
+                clusters[temp.tolist().index(min(temp))].append(point.tolist())
+
+            calculated = np.zeros_like(clusterCen)
+
+            for c in clusters:
+                center = np.zeros(self.s.shape[1])
+                for p in c:
+                    center += p
+                center = np.divide(center, len(c))
+                calculated[clusters.index(c)] = center
+            
+            if clusterCen.tolist() == calculated.tolist():
+                clusterCen = calculated
+                check = False
+            else:
+                clusterCen = calculated
+
+        return (clusterCen, clusters)
         
-    return calculated
-
-
-def kmeans(k, s):
-    check = True
-    clusterCen = []
-    clusters = [[],[],[],[],[]]
-    for j in range(1, k+1):
-        clusterCen.append(random.choice(s))
-    while check:
-        for x in s:
-            temp = []
-            for c in clusterCen:
-                dis = euclid_dist(x, c)
-                temp.append(dis)
-            clusters[temp.index(min(temp))].append(x)
-        if calc_clusterCent(clusters, k) == clusterCen:
-            clusterCen = calc_clusterCent(clusters, k)
-            check = False
-        else:
-            clusterCen = calc_clusterCent(clusters, k)
-
-    return (clusterCen, clusters)
-
-im = imageio.v2.imread('testimg.jpg')
+im = Image.open("testimg.jpg")
+im = np.array(im)
 
 im = im.reshape(im.shape[0]*im.shape[1],im.shape[2])
 
 # first element is the cluster centers and the second element is the clusters themselves
 
-final_clusters = kmeans(5, im.tolist())
+kmean = KMean(5, im)
+final_clusters = kmean.clustering()
 
 print(final_clusters[0])
